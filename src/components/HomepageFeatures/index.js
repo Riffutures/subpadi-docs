dc40fa5faeab38dd41b32cab2357dafab0c8ef32
Ayobami <alaran.ayobami@riffutures.com>
2023-12-13 17:55:33 +0100

    add links to index page navigation

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,6 +6,7 @@ import Link from '@docusaurus/Link';
 const FeatureList = [
   {
     title: 'Quickstart',
+    url: 'docs/intro',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -14,31 +15,31 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Authenticate',
+    url: 'docs/category/authentication',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Get Authenticated To Use The API Services
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Blog',
+    url: 'blog',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Check out our blog for tutorials on api usage and integrationn
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Svg, title, description, url}) {
   return (
     <div className={clsx('col col--4')}>
-      <Link to={'#'} className={styles.decorator}>
+      <Link to={url} className={styles.decorator}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
